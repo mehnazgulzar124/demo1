038ee2415c11edf91bc4681af6d6522d2e78c865
mehnazgulzar124 <135806179+mehnazgulzar124@users.noreply.github.com>
2025-10-28 00:25:48 +0500

    Updated heading text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,10 @@ function Home() {
     <div className="page">
       <h1>Welcome to the Home Page</h1>
       <p>This is a simple React project example.</p>
+      <h1>Welcome to Mehnaz’s React App</h1>
+
     </div>
+
   );
 }
 
